afd574c32b47ceef1766d04ebd2980cf121bd301
heikkiseppanen <6179953+heikkiseppanen@users.noreply.github.com>
2024-02-08 14:51:20 +0200

    Moved keyboard language toggle to fish launch

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     export PATH="$PATH"":/home/hege/.local/bin"
+
+    setxkbmap -layout us,fi -option grp:rctrl_toggle
 end
